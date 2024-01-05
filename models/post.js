9d718092ecf4a415470339fed0a5351aab3c93db
endulum <71853916+endulum@users.noreply.github.com>
2024-01-04 23:45:45 -0800

    modelled post schema, with helper queries

--- models/post.js
+++ models/post.js
@@ -1,48 +1,44 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({
-  author: {
-    type: Schema.ObjectId,
-    ref: 'Author',
-    required: true
-  },
-
-  title: {
-    type: String,
-    required: true,
-    // todo: min and max lengths?
-  },
-
-  subtitle: {
-    type: String,
-    required: true,
-    // todo: min and max lengths?
-  },
-
-  text: {
-    type: String, 
-    // todo: this type will change if blog posts are written in html or rich text
-    required: true,
-    // todo: min and max lengths?
-  },
-
-  timestamp: {
-    type: Date,
-    required: true,
-    default: Date.now
-  },
-
-  isVisible: {
-    type: Boolean,
-    required: true,
-    default: false
-  },
-
-  comments: [{
-    type: Schema.ObjectId,
-    ref: 'Comment',
-  }],
+const  PostSchema = new Schema({
+  author: { type: Schema.ObjectId, ref: 'Author' },
+  title: { type: String, required: true, minLength: 2, maxLength: 32 },
+  subtitle: { type: String, required: true, minLength: 2, maxLength: 64 },
+  content: { type: String, required: true, minLength: 2, maxLength: 2048 },
+  datePosted: { type: Date, default: () => Date.now(), immutable: true },
+  lastEdited: { type: Date },
+  comments: [{ type: Schema.ObjectId, ref: 'Comment' }]
 });
+
+PostSchema.query.byIdOrTitle = function(idOrTitle) {
+  if (mongoose.isValidObjectId(idOrTitle)) 
+    return this.findOne({ _id: idOrTitle });
+  else  
+    return this.findOne({ title: idOrTitle });
+};
+
+PostSchema.query.byParams = function(params) {
+  // assign default sort direction if the provided one is not valid or there is no provided one
+  if (
+    !params.sortDirection ||
+    !['ascending', 'descending', 'asc', 'desc', -1, 1].includes(params.sortDirection)
+  ) { params.sortDirection = 'ascending' }
+  // porgramatically build query as an object
+  const query = {};
+  if (params.title) query.title = { "$regex": params.title, "$options": "i" };
+  // programmatically build sort as an object
+  const sort = {};
+  switch (params.sortBy) {
+    case 'datePosted': sort.datePosted = params.sortDirection; break;
+    case 'title': sort.title = params.sortDirection; break;
+    case 'commentCount': sort.postCount = params.sortDirection; break;
+  };
+  // combine objects into the resulting query chain
+  if (params.populateComments)
+    return this.find(query).sort(sort).limit(params.limit).populate('comments');
+  else
+    return this.find(query).sort(sort).limit(params.limit);
+};
 
 export default mongoose.model('Post', PostSchema);
